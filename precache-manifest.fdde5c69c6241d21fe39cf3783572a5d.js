0c24c03d3c4d35b5fad3aa0dda1a1f3baf852221
Boris Kwok <bkwok123@gmail.com>
2020-09-22 16:25:54 -0600

    Updates

---
+++ precache-manifest.fdde5c69c6241d21fe39cf3783572a5d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e055939d520a801ce38be078a5ac3d8",
+    "url": "/cohort4-reactcrud/index.html"
+  },
+  {
+    "revision": "c1553c004f28bda194ab",
+    "url": "/cohort4-reactcrud/static/css/main.86157ef1.chunk.css"
+  },
+  {
+    "revision": "04530c8892523602199f",
+    "url": "/cohort4-reactcrud/static/js/2.b8efea11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cohort4-reactcrud/static/js/2.b8efea11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1553c004f28bda194ab",
+    "url": "/cohort4-reactcrud/static/js/main.c004c8be.chunk.js"
+  },
+  {
+    "revision": "537e52746115b991278d",
+    "url": "/cohort4-reactcrud/static/js/runtime-main.c5a8a73a.js"
+  },
+  {
+    "revision": "4d9b8bfc9ba475c2dbf2ca3fac142519",
+    "url": "/cohort4-reactcrud/static/media/book.4d9b8bfc.svg"
+  },
+  {
+    "revision": "074d76c99ad82a7589f3e0ff3fe9cea3",
+    "url": "/cohort4-reactcrud/static/media/face-det.074d76c9.jpg"
+  },
+  {
+    "revision": "796ef64a8050a2b03da5f1144ae8bca2",
+    "url": "/cohort4-reactcrud/static/media/face-recognition.796ef64a.svg"
+  },
+  {
+    "revision": "8d6500df777255268f7a5151d365e104",
+    "url": "/cohort4-reactcrud/static/media/graph.8d6500df.svg"
+  },
+  {
+    "revision": "84fe8e48f81214dffed722c1b00b84d1",
+    "url": "/cohort4-reactcrud/static/media/media.84fe8e48.svg"
+  },
+  {
+    "revision": "3ea8f236c7c8bc13ab96bf5c11be581b",
+    "url": "/cohort4-reactcrud/static/media/pic-frame1.3ea8f236.jpg"
+  },
+  {
+    "revision": "9bf9c5d337f57b8117cb408aa3f70bc8",
+    "url": "/cohort4-reactcrud/static/media/theme.9bf9c5d3.svg"
+  }
+]);
